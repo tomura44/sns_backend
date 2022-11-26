58c393a970bd3ea079bf997a70c7740ff3e534b4
daiki <daiki.20000727@docomo.ne.jp>
2022-11-26 17:16:36 +0900

    mongodbとの接続

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//データベース接続
+mongoose.connect(process.env.MONGOURL).then(() => {
+    console.log("connecting...")
+}).catch((err) => {
+    console.log(err)
+});
 
 //ミドルウェア
 app.use("/api/users", userRoute);
